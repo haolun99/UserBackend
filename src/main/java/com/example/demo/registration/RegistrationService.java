933c399a1a190e22319c17f348ff18641d0278b0
haolun99 <1204926758@qq.com>
2021-05-06 22:06:18 -0500

    new change

--- src/main/java/com/example/demo/registration/RegistrationService.java
+++ src/main/java/com/example/demo/registration/RegistrationService.java
@@ -1,11 +1,33 @@
 package com.example.demo.registration;
 
+import com.example.demo.appuser.AppUser;
+import com.example.demo.appuser.AppUserRole;
+import com.example.demo.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
-    public String register(RegistrationRequest request) {
 
-        return "it works";
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
+    public String register(RegistrationRequest request) {
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+
+        if (!isValidEmail) {
+            throw new IllegalStateException("Email not valid");
+        }
+
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
